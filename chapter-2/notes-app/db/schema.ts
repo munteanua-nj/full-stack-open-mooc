e7aae2d16d3ebe04afe9cc3d5aa6d241d019fec9
munteanua-nj <munteanua@gmail.com>
2026-06-10 19:15:36 -0400

    Add new users table

--- chapter-2/notes-app/db/schema.ts
+++ chapter-2/notes-app/db/schema.ts
@@ -4,4 +4,11 @@ export const notes = pgTable('notes', {
   id: serial('id').primaryKey(),
   content: text('content').notNull(),
   important: boolean('important').notNull().default(false),
+  userId: integer('user_id').reference(() => users.id),
+})
+
+export const users = pgTable('users', {
+  id: serial('id').primaryKey(),
+  username: text('username').notNull().unique(),
+  name: text('name').notNull(),
 })
