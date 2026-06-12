f556f177c41833fd0983e63c5c9ffb681ae8e85f
munteanua-nj <munteanua@gmail.com>
2026-06-11 22:21:27 -0400

    Fix notes service addNote().

--- chapter-2/notes-app/app/services/notes.ts
+++ chapter-2/notes-app/app/services/notes.ts
@@ -1,6 +1,6 @@
-import { eq } from 'drizzle-orm'
+import { eq, sql } from 'drizzle-orm'
 import { db } from '../../db'
-import { notes } from '../../db/schema'
+import { notes, users } from '../../db/schema'
 
 export const getNotes = async (importantOnly: boolean = false) => {
   if (importantOnly) {
@@ -19,7 +19,13 @@ export const getNoteById = async (id: number) => {
 }
 
 export const addNote = async (content: string, important: boolean) => {
-  await db.insert(notes).values({ content, important })
+  const user = await db.query.users.findFirst({
+    orderBy: sql`RANDOM()`,
+  })
+
+  if (user) {
+		await db.insert(notes).values({ content, important, userId: user.id })
+	}
 }
 
 export const toggleImportance = async (id: number) => {
