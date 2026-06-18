e81b98a34f3d631ea68aab0685f2ca88c484c09d
munteanua-nj <munteanua@gmail.com>
2026-06-18 19:21:26 -0400

    Empty app

---
+++ chapter-2/blog-app/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div>
+      <h2>blog app (source: 
+        <a href='https://github.com/munteanua-nj/full-stack-open-mooc/tree/main/chapter-2/blog-app' className='text-blue-600 underline hover:text-blue-800'>
+          https://github.com/munteanua-nj/full-stack-open-mooc/tree/main/chapter-2/blog-app
+        </a>)
+      </h2>
+    </div>
+  );
+}
