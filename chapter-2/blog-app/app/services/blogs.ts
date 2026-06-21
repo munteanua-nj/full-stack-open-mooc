9c4cda1af0a475be6834f40e671fe0039ac2061a
munteanua-nj <munteanua@gmail.com>
2026-06-20 23:29:24 -0400

    Return blog list sorted from most likes to least

--- chapter-2/blog-app/app/services/blogs.ts
+++ chapter-2/blog-app/app/services/blogs.ts
@@ -1,3 +1,11 @@
+interface Blog {
+  id: number;
+  title: string;
+  author: string;
+  url: string;
+  likes: number;
+}
+
 const blogs = [
   { id: 1, title: 'The Power Vertical', author: 'Brian Whitmore', url: 'https://brianwhitmore.substack.com/', likes: 0 },
   { id: 2, title: 'The Long Game', author: 'Jake Sullivan & Jon Finer', url: 'https://thelonggame.substack.com/', likes: 0 },
@@ -10,11 +18,11 @@ export const addBlog = (title: string, author: string, url: string, likes: numbe
   blogs.push({ id: nextId++, title, author, url, likes })
 }
 
-export const getBlogs = () => {
-  return blogs
+export const getBlogs = (): Blog[] => {
+  return [...blogs].sort((a, b) => b.likes - a.likes)
 }
 
-export const getBlogById = (id: number) => {
+export const getBlogById = (id: number): Blog | undefined => {
   return blogs.find((blog) => blog.id === id)
 }
 
