d7121ceaf430454307f03116db058b7cb54b9572
munteanua-nj <munteanua@gmail.com>
2026-06-22 16:26:29 -0400

    Add a version text to the homepage so I can identify it when deployed on the web.

--- chapter-2/blog-app/app/page.tsx
+++ chapter-2/blog-app/app/page.tsx
@@ -1,11 +1,12 @@
 export default function Home() {
   return (
     <div>
-      <h2>blog app (source: 
+      <h3>blog app (source: 
         <a href='https://github.com/munteanua-nj/full-stack-open-mooc/tree/main/chapter-2/blog-app' className='text-blue-600 underline hover:text-blue-800'>
           https://github.com/munteanua-nj/full-stack-open-mooc/tree/main/chapter-2/blog-app
         </a>)
-      </h2>
+      </h3>
+      Ver. 2.0+
     </div>
   )
 }
