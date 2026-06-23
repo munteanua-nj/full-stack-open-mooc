71eacf0c59913d4789668c076f4d623dcc352a1b
munteanua-nj <munteanua@gmail.com>
2026-06-23 12:29:35 -0400

    Add a new table users

--- chapter-2/blog-app/db/schema.ts
+++ chapter-2/blog-app/db/schema.ts
@@ -1,9 +1,18 @@
 import { pgTable, serial, text, integer } from 'drizzle-orm/pg-core'
 
+export const users = pgTable('users', {
+  id: serial('id').primaryKey(),
+  username: text('username').notNull().unique(),
+  name: text('name').notNull(),
+})
+
 export const blogs = pgTable('blogs', {
   id: serial('id').primaryKey(),
   title: text('title').notNull(),
   author: text('author').notNull(),
   url: text('url').notNull(),
   likes: integer('likes').default(0).notNull(),
+  userId: integer('user_id')
+    .notNull()
+    .references(() => users.id),
 })
